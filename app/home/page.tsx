116e6aa82b037d861f60ac74aec6b79fe8d62ee2
caihongtai <caihongtai>
2025-03-05 11:21:19 +0800

    图片资源的增加

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,22 +2,18 @@
  * @Author: caihongtai caihongtai
  * @Date: 2025-01-23 17:45:00
  * @LastEditors: caihongtai caihongtai
- * @LastEditTime: 2025-01-23 17:47:53
+ * @LastEditTime: 2025-03-04 15:36:39
  * @FilePath: /blxdropship/app/home/page.tsx
  * @Description: Home page component
  */
 import Hero from '@/components/Hero'
 import Services from '@/components/Services'
-import ERPSystem from '@/components/ERPSystem'
-import LogisticsPartners from '@/components/LogisticsPartners'
 
 export default function Home() {
   return (
     <div className='min-h-screen'>
       <Hero />
       <Services />
-      <ERPSystem />
-      <LogisticsPartners />
     </div>
   )
 }
